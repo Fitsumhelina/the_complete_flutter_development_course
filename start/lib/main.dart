9ac782cf8d11e9eefddee7eb8b834f28189f0666
fitsumhelina <dev.fitsum@gmail.com>
2025-12-05 22:37:16 +0300

    Fix home page reference and update scaffold background color in main.dart

--- start/lib/main.dart
+++ start/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:start/pages/home.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,9 +13,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        scaffoldBackgroundColor: const Color.fromARGB(255, 2, 199, 91),
+        scaffoldBackgroundColor: const Color.fromARGB(255, 126, 128, 127),
       ),
-      home: (),
+      home: HomePage(),
     );
   }
 }
